20166a03b8540d09cbee93e52669aecd2c5e59cc
ckwong <chunkang.wong94@gmail.com>
2022-11-07 11:30:24 +0800

    Removed redundant store.dispatch in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import { fetchSignInStatus } from "./features/portal/portalSlice";
 import "./index.css";
 import store from "./store/store";
 
 async function main() {
-  store.dispatch(fetchSignInStatus());
-
   ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
       <Provider store={store}>
